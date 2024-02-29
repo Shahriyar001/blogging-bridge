c99a1ec17923f31a859a117f630e4d7edb33addb
Shahriyar001 <shahriyarmahbub55@gmail.com>
2024-02-29 11:49:05 +0600

    side menu componant design complite

--- app/componants/Menu/Menu.jsx
+++ app/componants/Menu/Menu.jsx
@@ -1,8 +1,93 @@
 import React from "react";
 import styles from "./Menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
-  return <div className={styles.container}>Menu</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <MenuPosts withImage={false} />
+      {/* //////// */}
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors pick</h1>
+      <MenuPosts withImage={true} />
+      {/* <div className={styles.items}>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe - </span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>
+              Fashion
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe - </span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>Food</span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe - </span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>
+              Culture
+            </span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing.
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe - </span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+      </div> */}
+    </div>
+  );
 };
 
 export default Menu;
